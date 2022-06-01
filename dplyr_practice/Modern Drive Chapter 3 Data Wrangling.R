d14a5be184122b6d37f37fbcdafe0c59167975da
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2022-06-01 06:24:45 -0400

    Update Modern Drive Chapter 3 Data Wrangling.R

--- dplyr_practice/Modern Drive Chapter 3 Data Wrangling.R
+++ dplyr_practice/Modern Drive Chapter 3 Data Wrangling.R
@@ -1,6 +1,5 @@
 # Going thru data wrangling exercises listed in Chapter 3 of Modern Drive https://moderndive.com/3-wrangling.html
 
-
 # Needed Packages ---------------------------------------------------------
 
 library(dplyr)
@@ -11,3 +10,21 @@ library(nycflights13)
 alaska_flights <- flights %>% 
   filter(carrier == "AS")
 View(flights)
+
+# Filter rows -------------------------------------------------------------
+
+# filter() function works similarly to excel allows to specify the criteria about the values of a variable and filters out only those values that match the criteria.
+
+portland_flights <- flights %>% 
+  filter(dest == "PDX")
+View(portland_flights)
+# filtering dataset to just see flights going to Portland
+
+btv_sea_flights_fall <- flights %>% 
+  filter(origin == "JFK" & (dest == "BTV" | dest == "SEA") & month >= 10)
+View(btv_sea_flights_fall)
+# filtering flights leaving from JFK with destination in Burlington, Vermont or Seattle, Washington in the months of October, November, or December
+
+btv_sea_flights_fall <- flights %>% 
+  filter(origin == "JFK", (dest == "BTV" | dest == "SEA"), month >= 10)
+View(btv_sea_flights_fall)
