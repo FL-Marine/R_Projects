80fd0906c8915ef7a0999f4bd6734083487fdd60
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2022-06-02 05:01:42 -0400

    Update Modern Drive Chapter 3 Data Wrangling.R

--- dplyr_practice/Modern Drive Chapter 3 Data Wrangling.R
+++ dplyr_practice/Modern Drive Chapter 3 Data Wrangling.R
@@ -20,6 +20,8 @@ portland_flights <- flights %>%
 View(portland_flights)
 # filtering dataset to just see flights going to Portland
 
+## and/or filtering & = and | = or ----
+
 btv_sea_flights_fall <- flights %>% 
   filter(origin == "JFK" & (dest == "BTV" | dest == "SEA") & month >= 10)
 View(btv_sea_flights_fall)
@@ -29,3 +31,31 @@ btv_sea_flights_fall <- flights %>%
   filter(origin == "JFK", (dest == "BTV" | dest == "SEA"), month >= 10)
 View(btv_sea_flights_fall)
 # same code as above^ just without the & symbol
+
+## not operator ! filtering ----
+not_BTV_SEA <- flights %>% 
+  filter(!(dest == "BTV" | dest == "SEA"))
+View(not_BTV_SEA)
+# filtering rows where flights Burlington, VT or Seattle, WA
+# Note where parentheses is around (dest == "BTV" | dest == "SEA")
+
+flights %>% filter(!dest == "BTV" | dest == "SEA")
+# This would result in df returning all flights NOT headed to BTV or SEA
+
+## returning large amount of results using %in% operator ----
+
+many_airports <- flights %>% 
+  filter(dest == "SEA" | dest == "SFO" | dest == "PDX" | 
+         dest == "BTV" | dest == "BDL")
+
+many_airports <- flights %>% 
+  filter(dest %in% c("SEA", "SFO", "PDX", "BTV", "BDL"))
+View(many_airports)
+# Both return the same result but more efficent approach is the use of the %in% operator along with c() function
+# c() combines or concatenates values into a single vector of values
+
+# As a final note, we recommend that filter() should often be among the first verbs you consider applying to your data. 
+# This cleans your dataset to only those rows you care about, or put differently, it narrows down the scope of your data frame to just the observations you care about.
+
+
+# summarize variables -----------------------------------------------------
